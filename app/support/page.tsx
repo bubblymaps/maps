e51a81e70f1656c81f111af18b272503a78bcee2
linuskang <linus@kang.id.au>
2025-11-21 08:04:35 +1000

    add support

---
+++ app/support/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Support() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push("https://github.com/bubblymaps/maps/wiki");
+    }, [router]);
+
+    return null;
+}
